c58a159042d90134b70380eb5961383d7352707d
Jolo Cansana <jcansana@gmail.com>
2021-03-27 01:46:08 +0800

    latest changes

--- loadNew.sh
+++ loadNew.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python3 loconotion https://www.notion.so/Jose-Lorenzo-Jolo-Cansana-5b7d50bcbfb64ef39d3b0231e36fdd18
+cd loconotion
+python3 loconotion https://www.notion.so/Jose-Lorenzo-Jolo-Cansana-5b7d50bcbfb64ef39d3b0231e36fdd18 --chromedriver chromedriver
+cd ..
 git add .
 git commit -m "latest changes"
 git push origin prod
